d5b78c1a5d4d597b2841c50fdbab7638f4a1e959
244954 <mateusz_m_malecki@interia.pl>
2020-10-11 16:37:56 +0200

    Draw simple skeletal board

--- app/src/main/java/com/example/kakuro/KakuroBoardView.kt
+++ app/src/main/java/com/example/kakuro/KakuroBoardView.kt
@@ -9,13 +9,42 @@ import android.view.View
 
 class KakuroBoardView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
 
-    private val linePaint = Paint().apply {
+    private var boardSize = 9 // like a sudoku board for now
+    private var cellSizePixels = 0F
+
+    private var selectedRow = -1
+    private var selectedColumn = -1
+
+    private val thickLinePaint = Paint().apply {
         style = Paint.Style.STROKE
         color = Color.DKGRAY
-        strokeWidth = 4F
+        strokeWidth = 6F
+    }
+
+    private val thinLinePaint = Paint().apply {
+        style = Paint.Style.STROKE
+        color = Color.DKGRAY
+        strokeWidth = 3F
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val sizePixels = Math.min(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(sizePixels, sizePixels)
+        // assume kakuros are always square
     }
 
     override fun onDraw(canvas: Canvas?) {
-        canvas?.drawRect(0F, 0F, width.toFloat(), width.toFloat(), linePaint)
+        cellSizePixels = (width / boardSize).toFloat()
+
+        drawLines(canvas)
+    }
+
+    private fun drawLines(canvas: Canvas?) {
+        canvas?.drawRect(0F, 0F, width.toFloat(), width.toFloat(), thickLinePaint)
+        for (i in 1 until boardSize) {
+            canvas?.drawLine(0F, i * cellSizePixels, width.toFloat(), i * cellSizePixels, thinLinePaint)
+            canvas?.drawLine(i * cellSizePixels, 0F, i * cellSizePixels, height.toFloat(), thinLinePaint)
+        }
     }
 }
