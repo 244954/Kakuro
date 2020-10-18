62f840eeb3479d6e1bb1103dc2f98faeff4f9e49
244954 <mateusz_m_malecki@interia.pl>
2020-10-18 16:57:53 +0200

    On Click

--- app/src/main/java/com/example/kakuro/KakuroBoardView.kt
+++ app/src/main/java/com/example/kakuro/KakuroBoardView.kt
@@ -5,6 +5,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class KakuroBoardView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
@@ -40,6 +41,16 @@ class KakuroBoardView(context: Context, attributeSet: AttributeSet) : View(conte
         strokeWidth = 3F
     }
 
+    private val selectedCellPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.parseColor(resources.getString(R.string.selectedCellColor))
+    }
+
+    private val errorCellPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        color = Color.parseColor(resources.getString(R.string.errorCellColor))
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         val sizePixels = Math.min(widthMeasureSpec, heightMeasureSpec)
@@ -50,7 +61,12 @@ class KakuroBoardView(context: Context, attributeSet: AttributeSet) : View(conte
     override fun onDraw(canvas: Canvas?) {
         cellSizePixels = (width / boardSize).toFloat()
 
+        fillCells(canvas)
         drawLines(canvas)
+    }
+
+    private fun fillCells(canvas: Canvas?) {
+        canvas?.drawRect(selectedColumn * cellSizePixels, selectedRow * cellSizePixels, (selectedColumn + 1) * cellSizePixels, (selectedRow + 1) * cellSizePixels, selectedCellPaint)
     }
 
     private fun drawLines(canvas: Canvas?) {
@@ -59,5 +75,21 @@ class KakuroBoardView(context: Context, attributeSet: AttributeSet) : View(conte
             canvas?.drawLine(0F, i * cellSizePixels, width.toFloat(), i * cellSizePixels, thinLinePaint)
             canvas?.drawLine(i * cellSizePixels, 0F, i * cellSizePixels, height.toFloat(), thinLinePaint)
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                handleTouchEvent(event.x, event.y)
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun handleTouchEvent(x: Float, y: Float) {
+        selectedRow = (y / cellSizePixels).toInt()
+        selectedColumn = (x / cellSizePixels).toInt()
+        invalidate() // draw again
     }
 }
